022eeaa45ae3e81c27cc1acf4c7cc7780d625447
chenjing <1134991001@qq.com>
2017-04-11 18:37:28 +0800

    增加线程池

--- src/test/java/com/warehouse/sellercube/ThreadTest.java
+++ src/test/java/com/warehouse/sellercube/ThreadTest.java
@@ -1,9 +1,9 @@
 package com.warehouse.sellercube;
 
-import com.warehouse.sellercube.config.ApplicationContextProvider;
-import com.warehouse.sellercube.thread.MoniotrTask;
+import com.warehouse.sellercube.thread.AsyncTaskService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,14 +13,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ThreadTest {
+    @Autowired
+    private AsyncTaskService asyncTaskService;
     @Test
     public void show()throws Exception{
-        MoniotrTask m1=ApplicationContextProvider.getBean("mTask", MoniotrTask.class);
+/*        MoniotrTask m1=ApplicationContextProvider.getBean("mTask", MoniotrTask.class);
         MoniotrTask m2=ApplicationContextProvider.getBean("mTask", MoniotrTask.class);
         MoniotrTask m3=ApplicationContextProvider.getBean("mTask", MoniotrTask.class);
         System.out.println(m1+" => "+m1.getName());
         System.out.println(m2+" => "+m2.getName());
-        System.out.println(m3+" => "+m3.getName());
+        System.out.println(m3+" => "+m3.getName());*/
+        for(int i =0 ;i<10;i++){
+            asyncTaskService.executeAsyncTask(i);
+            asyncTaskService.executeAsyncTaskPlus(i);
+        }
 
     }
 }
